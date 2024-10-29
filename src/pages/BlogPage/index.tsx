2388018002a4e9686404635220ec17960e0ceba8
Anton <ivanovdevelop@yandex.ru>
2024-10-29 19:34:55 +0300

    change favicon

--- src/pages/BlogPage/index.tsx
+++ src/pages/BlogPage/index.tsx
@@ -1,7 +1,11 @@
+import netrunner from "../../assets/images/netrunner.png"
+import "./index.scss"
+
 function Blog() {
   return (
-    <div>
+    <div className="blog">
       <h2>Blog</h2>
+      <img src={netrunner} />
     </div>
   );
 }
