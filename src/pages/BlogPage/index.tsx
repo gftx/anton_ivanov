c13033aaeaa90df157cba1e9b48f7b547b7fdef3
Anton <ivanovdevelop@yandex.ru>
2024-11-07 12:05:40 +0300

    wip

--- src/pages/BlogPage/index.tsx
+++ src/pages/BlogPage/index.tsx
@@ -3,7 +3,6 @@ import "./index.scss"
 function Blog() {
   return (
     <div className="blog">
-      <h2>Blog</h2>
       {/*<img src={netrunner} />*/}
       <h3>work in progress...</h3>
     </div>
