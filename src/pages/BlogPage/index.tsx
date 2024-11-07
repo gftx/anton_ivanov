7a8fba72ac925d084aa70c20c6d5b9504cd68c2a
Anton <ivanovdevelop@yandex.ru>
2024-11-07 11:32:01 +0300

    wip on blog page

--- src/pages/BlogPage/index.tsx
+++ src/pages/BlogPage/index.tsx
@@ -1,11 +1,11 @@
-import netrunner from "../../assets/images/netrunner.png"
 import "./index.scss"
 
 function Blog() {
   return (
     <div className="blog">
       <h2>Blog</h2>
-      <img src={netrunner} />
+      {/*<img src={netrunner} />*/}
+      <h3>work in progress...</h3>
     </div>
   );
 }
