221ab58fcb7b7aa85dbe04a1fcf4561d6e72532a
Anton <ivanovdevelop@yandex.ru>
2025-01-17 12:28:45 +0300

    rebuild

--- src/i18n.js
+++ src/i18n.js
@@ -16,15 +16,14 @@ i18next.use(initReactI18next).init({
 
         experienceList: [
           {
-            title: "Fit Heroes",
-            data: "01.2024 - 11.2024",
+            title: "Raiffeisen Bank",
+            data: "01.2024 - present",
             bullets: [
-              "Разработка децентрализованных приложений (dApps): Создавал интерактивные интерфейсы для Web3-платформ с использованием React, TypeScript и библиотеки управления состоянием Redux.",
-              "Интеграция с блокчейном: Настраивал взаимодействие с Ethereum через Web3.js. Обеспечивал безопасное подключение кошельков (MetaMask).",
-              "Реализация пользовательских интерфейсов: Разрабатывал адаптивные и отзывчивые дизайны с использованием Styled Components и Material-UI. Оптимизировал производительность интерфейса для быстрой загрузки и плавной работы.",
-              "Интеграция смарт-контрактов: Подключал смарт-контракты к интерфейсу приложения, реализовывал функции чтения данных из блокчейна и отправки транзакций. Тестировал взаимодействия с помощью Truffle."
+              "developed microfrontend system based on webpack module federation, which improved the scalability of the project and also allowed the team to work independently of each other",
+              "I created mockups in figma myself to improve UX and meet the customer's needs with their subsequent implementation on the frontend",
+              "add unit tests on Jest and Testing Library to the project with almost full coverage of most important components"
             ],
-            link: "https://www.rbinternational.com/de/raiffeisen.html",
+            link: "https://www.raiffeisen.ru/",
           },
           {
             title: "Garpix",
@@ -61,13 +60,12 @@ i18next.use(initReactI18next).init({
 
         experienceList: [
           {
-            title: "Fit Heroes",
-            data: "01.2024 - 11.2024",
+            title: "Raiffeisen Bank",
+            data: "01.2024 - present",
             bullets: [
-              "Developed decentralized applications (dApps): Built interactive interfaces for Web3 platforms using React, TypeScript, and state management library Redux.",
-              "Blockchain integration: Configured interactions with Ethereum through Web3.js. Enabled secure wallet connections (MetaMask)",
-              "Implemented user interfaces: Created responsive and adaptive designs using Styled Components and Material-UI. Optimized UI performance for fast loading and seamless interaction",
-              "Smart contract integration: Connected smart contracts to the application interface, implemented functions for reading blockchain data and sending transactions. Tested interactions using Truffle"
+              "developed microfrontend system based on webpack module federation, which improved the scalability of the project and also allowed the team to work independently of each other",
+              "I created mockups in figma myself to improve UX and meet the customer's needs with their subsequent implementation on the frontend",
+              "add unit tests on Jest and Testing Library to the project with almost full coverage of most important components"
             ],
             link: "https://www.raiffeisen.ru/",
           },
